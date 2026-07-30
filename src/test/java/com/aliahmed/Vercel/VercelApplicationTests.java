a709c8ba1b21c5d689bd5b06ada6582907234ed3
aliahmed235 <aliahmed1174@outlook.com>
2026-07-30 22:58:26 +0300

    Initial commit

---
+++ src/test/java/com/aliahmed/Vercel/VercelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliahmed.Vercel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VercelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
