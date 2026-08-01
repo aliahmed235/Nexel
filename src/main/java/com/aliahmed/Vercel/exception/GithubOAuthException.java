6c2fa9974aa4d90d755b0a29801e4beaf40d0918
aliahmed235 <aliahmed1174@outlook.com>
2026-08-01 12:20:08 +0300

    Add GitHub OAuth login, layered structure, Dockerfile

---
+++ src/main/java/com/aliahmed/Vercel/exception/GithubOAuthException.java
@@ -0,0 +1,12 @@
+package com.aliahmed.Vercel.exception;
+
+public class GithubOAuthException extends RuntimeException {
+
+    public GithubOAuthException(String message) {
+        super(message);
+    }
+
+    public GithubOAuthException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
