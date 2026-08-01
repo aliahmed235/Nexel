a4340dfde0fe7e448d361bf22882ebc53f9bb31e
aliahmed235 <aliahmed1174@outlook.com>
2026-08-01 12:18:21 +0300

    Add GitHub OAuth login, layered structure, Dockerfile

--- src/main/java/com/aliahmed/Vercel/VercelApplication.java
+++ src/main/java/com/aliahmed/Vercel/VercelApplication.java
@@ -2,8 +2,10 @@ package com.aliahmed.Vercel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class VercelApplication {
 
 	public static void main(String[] args) {
