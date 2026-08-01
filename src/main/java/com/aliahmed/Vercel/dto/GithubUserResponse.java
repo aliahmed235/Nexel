7857936db3fa4d52ba914e20f81a9146eecf5aca
aliahmed235 <aliahmed1174@outlook.com>
2026-08-01 12:20:22 +0300

    Add GitHub OAuth login, layered structure, Dockerfile

---
+++ src/main/java/com/aliahmed/Vercel/dto/GithubUserResponse.java
@@ -0,0 +1,14 @@
+package com.aliahmed.Vercel.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record GithubUserResponse(
+        Long id,
+        String login,
+        String name,
+        String email,
+        @JsonProperty("avatar_url") String avatarUrl
+) {
+}
